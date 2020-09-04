cc2619006a0c650b5fdecaab74d2fb13aa0a3c37
Nick Mallas <nmallas23@gmail.com>
2020-09-04 00:14:36 -0500

    Add feature image title and css changes

--- client/src/components/FeatureImage.js
+++ client/src/components/FeatureImage.js
@@ -12,10 +12,10 @@ class FeatureImage extends React.Component {
     randomNum = length => Math.floor(Math.random() * length)
 
     async componentDidMount() {
-
         let res = await fetch(`/api/content/ctv`);
         if(res.ok) {
             let data = await res.json();
+            console.log(data);
             let randomIndex = this.randomNum(data.length - 1);
             this.setState({currentVid: data[randomIndex]});
         }
@@ -25,15 +25,17 @@ class FeatureImage extends React.Component {
         let vid = this.state.currentVid;
         return  (
             <div id="feature-container">
+                <h1 className="feature-title"> {vid.original_name} </h1>
                 {!vid?.backdrop_path ? "" :
                     <img
                         className="feature-image"
                         src={`https://image.tmdb.org/t/p/original/${vid.backdrop_path}`}
-                        alt={vid.orignal_name}
+                        alt={vid.original_name}
                         data-id={vid.id}
                         data-poster={vid.poster_path}
                     />
                 }
+
             </div>
         )
     }
